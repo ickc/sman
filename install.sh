d19ed2f5b143fc6e1f209fb5e2638b30aaf7354c
Kolen Cheung <christian.kolen@gmail.com>
2017-11-05 01:40:28 -0800

    install.sh: cleanup syntax

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 
 version=0.1.0
 
-download() {
-    if [ ! -d ~/.sman/ ]; then
+download(){
+    if [[ ! -d ~/.sman/ ]]; then
         if command -v git > /dev/null; then
             git clone https://github.com/ickc/sman.git ~/.sman/
         else
@@ -13,7 +13,7 @@ download() {
             return
         fi
     fi
-    [ -d ~/.sman/bin ] || mkdir ~/.sman/bin;
+    [[ -d ~/.sman/bin ]] || mkdir ~/.sman/bin
     cd ~/.sman/bin
     local url=https://github.com/ickc/sman/releases/download/$version/${1}.tgz
     if command -v curl > /dev/null; then
@@ -25,27 +25,27 @@ download() {
         return
     fi
 
-    if [ ! -f $1 ]; then
+    if [[ ! -f $1 ]]; then
         binary_error="Failed to download ${1}"
         return
     fi
 
-    mv ${1} sman; chmod +x sman
+    mv ${1} sman
+    chmod +x sman
 }
 
-
-ask() {
+ask(){
     # If stdin is a tty, we are "interactive".
     # non-interactive shell: wait for a linefeed
     #     interactive shell: continue after a single keypress
-    read_n=$([ -t 0 ] && echo "-n 1")
+    read_n=$([[ -t 0 ]] && echo "-n 1")
 
     read -p "$1 ([y]/n) " $read_n -r
     echo
     [[ $REPLY =~ ^[Nn]$ ]]
 }
 
-append_line() {
+append_line(){
     set -e
 
     local update line file pat lno
@@ -56,16 +56,16 @@ append_line() {
 
     echo "Update $file:"
     echo "  - $line"
-    [ -f "$file" ] || touch "$file"
-    if [ $# -lt 4 ]; then
+    [[ -f "$file" ]] || touch "$file"
+    if [[ $# -lt 4 ]]; then
         lno=$(\grep -nF "$line" "$file" | sed 's/:.*//' | tr '\n' ' ')
     else
         lno=$(\grep -nF "$pat" "$file" | sed 's/:.*//' | tr '\n' ' ')
     fi
-    if [ -n "$lno" ]; then
+    if [[ -n "$lno" ]]; then
         echo "    - Already exists: line #$lno"
     else
-        if [ $update -eq 1 ]; then
+        if [[ $update -eq 1 ]]; then
             echo >> "$file"
             echo "$line" >> "$file"
             echo "    + Added"
@@ -89,8 +89,8 @@ case "$archi" in
   *)              binary_available=0 binary_error=1  ;;
 esac
 
-if [ -n "$binary_error" ]; then
-    if [ $binary_available -eq 0 ]; then
+if [[ -n "$binary_error" ]]; then
+    if [[ $binary_available -eq 0 ]]; then
         echo "No prebuilt binary for $archi ..."
     fi
     echo "  - $binary_error !!!"
@@ -102,18 +102,18 @@ echo
 ask "Do you want to update your shell configuration files?"
 update_config=$?
 has_zsh=$(command -v zsh > /dev/null && echo 1 || echo 0)
-shells=$([ $has_zsh -eq 1 ] && echo "bash zsh" || echo "bash")
+shells=$([[ $has_zsh -eq 1 ]] && echo "bash zsh" || echo "bash")
 for shell in $shells; do
-    [ $shell = zsh ] && dest=${ZDOTDIR:-~}/.zshrc || dest=~/.bashrc
-    append_line $update_config "[ -f ~/.sman/sman.rc ] && source ~/.sman/sman.rc" "$dest" "~/.sman/sman.rc"
+    [[ $shell = zsh ]] && dest=${ZDOTDIR:-~}/.zshrc || dest=~/.bashrc
+    append_line $update_config "[[ -f ~/.sman/sman.rc ]] && source ~/.sman/sman.rc" "$dest" "~/.sman/sman.rc"
     append_line $update_config 'export PATH=$PATH:~/.sman/bin' "$dest" '$PATH:~/.sman/bin'
 done
 
 # Snippets from repo
-if [ ! -f ~/snippets/ ]; then
+if [[ ! -f ~/snippets/ ]]; then
     echo
     ask "Copy snippets from repo to home?"
-    if [ $? -eq 1 ]; then
+    if [[ $? -eq 1 ]]; then
         cp -r ~/.sman/snippets ~
     fi
 fi
