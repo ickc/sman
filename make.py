0d240620e7be3c6b1edb3417f19f1fe2b6c04aa2
Kolen Cheung <christian.kolen@gmail.com>
2020-05-28 19:05:06 -0700

    Create make.py

---
+++ make.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+'''generate all combinations of supported GOOS and GOARCH
+
+hint: run $(./make.py)
+'''
+
+import pandas as pd
+
+
+def main():
+    for _, row in pd.read_html('https://golang.org/doc/install/source')[0].iterrows():
+        print(f"make build GOOS={row['$GOOS']} GOARCH={row['$GOARCH']}")
+
+if __name__ == "__main__":
+    main()
