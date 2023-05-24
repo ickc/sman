f751bb92ef701c5641d3ba1862a27ae9e93eac8a
Kolen Cheung <christian.kolen@gmail.com>
2023-05-24 16:11:58 +0100

    remove functionality that modifies rc files

--- install.sh
+++ install.sh
@@ -35,49 +35,6 @@ download(){
     chmod +x sman
 }
 
-ask(){
-    # If stdin is a tty, we are "interactive".
-    # non-interactive shell: wait for a linefeed
-    #     interactive shell: continue after a single keypress
-    read_n=$([[ -t 0 ]] && echo "-n 1")
-
-    read -p "$1 ([y]/n) " $read_n -r
-    echo
-    [[ $REPLY =~ ^[Nn]$ ]]
-}
-
-append_line(){
-    set -e
-
-    local update line file pat lno
-    update="$1"
-    line="$2"
-    file="$3"
-    pat="${4:-}"
-
-    echo "Update $file:"
-    echo "  - $line"
-    [[ -f "$file" ]] || touch "$file"
-    if [[ $# -lt 4 ]]; then
-        lno=$(\grep -nF "$line" "$file" | sed 's/:.*//' | tr '\n' ' ')
-    else
-        lno=$(\grep -nF "$pat" "$file" | sed 's/:.*//' | tr '\n' ' ')
-    fi
-    if [[ -n "$lno" ]]; then
-        echo "    - Already exists: line #$lno"
-    else
-        if [[ $update -eq 1 ]]; then
-            echo >> "$file"
-            echo "$line" >> "$file"
-            echo "    + Added"
-        else
-            echo "    ~ Skipped"
-        fi
-    fi
-    echo
-    set +e
-}
-
 # Try to download binary executable
 archi=$(uname -sm)
 binary_available=1
@@ -103,17 +60,3 @@ if [[ -n "$binary_error" ]]; then
     echo "  - $binary_error !!!"
     exit 1
 fi
-
-# Append sman.rc to rc
-echo
-ask "Do you want to update your shell configuration files?"
-update_config=$?
-has_zsh=$(command -v zsh > /dev/null && echo 1 || echo 0)
-shells=$([[ $has_zsh -eq 1 ]] && echo "bash zsh" || echo "bash")
-for shell in $shells; do
-    [[ $shell = zsh ]] && dest=${ZDOTDIR:-~}/.zshrc || dest=~/.bashrc
-    append_line $update_config "[[ -f ~/.sman/sman.rc ]] && source ~/.sman/sman.rc" "$dest" "$HOME/.sman/sman.rc"
-    append_line $update_config 'export SMAN_SNIPPET_DIR=~/.sman/snippets' "$dest" "$HOME/.sman/snippets"
-done
-
-echo "Done. Logout or reload your rc"
