df96d1733a9f36fae4c1012966ae425b810b544e
Kolen Cheung <christian.kolen@gmail.com>
2024-05-17 16:21:08 +0100

    rewrite to run and write the stderrs

--- make.py
+++ make.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 
-'''generate all combinations of supported GOOS and GOARCH
+"""generate all combinations of supported GOOS and GOARCH
+"""
 
-hint: run
-
-$(./make.py)
-
-or using parallel
-
-./make.py | parallel -j 10
-'''
+import subprocess
+from concurrent.futures import ProcessPoolExecutor as Executor
+from pathlib import Path
 
 import pandas as pd
 
 
+def make(GOOS: str, GOOARCH: str) -> None:
+    args = ["make", "build", f"GOOS={GOOS}", f"GOARCH={GOOARCH}"]
+    print(subprocess.list2cmdline(args))
+    res = subprocess.run(
+        args,
+        check=False,
+        capture_output=True,
+    )
+    with Path(f"{GOOS}_{GOOARCH}.stdout").open("wb") as f:
+        f.write(res.stdout)
+    with Path(f"{GOOS}_{GOOARCH}.stderr").open("wb") as f:
+        f.write(res.stderr)
+
+
 def main():
-    for _, row in pd.read_html('https://golang.org/doc/install/source')[0].iterrows():
-        print(f"make build GOOS={row['$GOOS']} GOARCH={row['$GOARCH']}")
+    envs = pd.read_html("https://golang.org/doc/install/source")[0][
+        ["$GOOS", "$GOARCH"]
+    ].T.values.tolist()
+    with Executor() as executor:
+        executor.map(make, *envs)
+
 
 if __name__ == "__main__":
     main()
